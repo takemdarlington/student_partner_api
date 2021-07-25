e72c70b875760bd672f1cdf2ea5d9df198e38fdb
takemdarlington <takemdarlington@gmail.com>
2021-07-25 18:20:52 +0100

    connect to mongodb

--- config.js
+++ config.js
@@ -4,8 +4,18 @@ const config = {
     port: process.env.PORT || 8080
   },
   mongo: {
-    url: process.env.MONGO_DB_URI || "mongodb+srv://adminuser:adminpassword@cluster0.yftuo.mongodb.net/test?retryWrites=true&w=majority"
+    url: process.env.MONGO_DB_URI || "mongodb+srv://adminuser:adminpassword@cluster0.yftuo.mongodb.net/studentpartnerdb?retryWrites=true&w=majority"
   }
 }
 
 module.exports = config
+
+
+// const { MongoClient } = require('mongodb');
+// const uri = "mongodb+srv://adminuser:<password>@cluster0.yftuo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
